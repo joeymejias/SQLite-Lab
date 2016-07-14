ddef845da776bf33f77d68402998971e2602cca4
Joey <joeymejias@gmail.com>
2016-07-13 20:30:58 -0400

    Santa Monica

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,17 +2,25 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+--SELECT * FROM `country` WHERE `continent` LIKE '%Europe%' ESCAPE '\' ORDER BY `population` ASC LIMIT 0, 50000;
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
+--SELECT * FROM `countrylanguage` WHERE `countrycode` LIKE '%VAT%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
+
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
+
+-- Query One
+---- SELECT * FROM `countrylanguage` WHERE `language` LIKE '%Italian%' ESCAPE '\' AND `isofficial` LIKE '%t%' ESCAPE '\' ORDER BY `percentage` DESC LIMIT 0, 50000;
+-- Query Two
+---- SELECT * FROM `country` WHERE `code` LIKE '%SMR%' ESCAPE '\' AND `continent` LIKE '%Europe%' ESCAPE '\' ORDER BY `region` ASC LIMIT 0, 50000;
 
 
 
@@ -21,17 +29,23 @@
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
+-- SELECT * FROM `city` WHERE `countrycode` LIKE '%SMR%' ESCAPE '\' ORDER BY `name` ASC LIMIT 0, 50000;
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+-- SELECT * FROM `city` WHERE `name` LIKE '%Serr%' ESCAPE '\' ORDER BY `countrycode` ASC LIMIT 0, 50000;
+
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
+
+-- SELECT * FROM `country` WHERE `code` LIKE '%BRA%' ESCAPE '\' ORDER BY `code` ASC LIMIT 0, 50000;
+-- SELECT * FROM `city` WHERE `id` = '211' ESCAPE '\' ORDER BY `countrycode` ASC LIMIT 0, 50000;
 
 
 
@@ -52,6 +66,6 @@
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+-- SELECT * FROM `city` WHERE `population` LIKE '%91084%' ESCAPE '\' ORDER BY `countrycode` ASC LIMIT 0, 50000;
 
-
--- She's in ____________________________!
+-- She's in Santa Monica!
